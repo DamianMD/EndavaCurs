bfb3bbd04719a8de71231ef6e96e69fbc60981c7
DamianMD <damian971202@gmail.com>
2019-03-20 19:11:05 +0200

    First commit

---
+++ Librarie/LibrarieOnline/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using LibrarieOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibrarieOnline.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+            ViewBag.Message = " Your application descrpiton page.";
+        }
+
+        public ActionResult Books()
+        {
+            var books = new List<Book>()
+            {
+                new Book ()
+                {
+                    Id = 1,
+                    Name = "Song of Ice and Fire",
+                    Author = "Josh Martin",
+                    Description = "Action/Fantasy"
+          
+                },
+                new Book()
+                {
+                    Id = 2,
+                    Name = " Php is thras and here is why",
+                    Author = "Valeriu Seremet",
+                    Description = "..."
+                }
+            };
+            return View("View",books);
+            
+            
+        }
+    }
+}
